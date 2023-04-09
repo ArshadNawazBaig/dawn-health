08a8afcacb27ab9fbbb21227c17a2f31c5acdd8c
arshadnawazbaig <arshadnawazbaig@gmail.com>
2023-04-10 04:44:45 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Route, Routes, useLoc, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Footer from './components/Footer';
 import { Header } from './components/Navbar';
 import useScroll from './hooks/useScroll';
